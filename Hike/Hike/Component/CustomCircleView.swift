ff58bb319e627159cb60203dbb63576522921929
martdealer <ios.martdealer@gmail.com>
2023-12-01 21:44:08 +0900

    feat : animation

---
+++ Hike/Hike/Component/CustomCircleView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by zsm on 12/1/23.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    @State private var isAnimatedGradient: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(colors: [ .customIndigoMedium, .customSalmonLight],
+                                   startPoint: isAnimatedGradient ? .topLeading : .bottomLeading,
+                                   endPoint: isAnimatedGradient ? .bottomTrailing : .topTrailing)
+                )
+                .onAppear {
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                        isAnimatedGradient.toggle()
+                    }
+                }
+            MotionAnimationView()
+        }
+        .frame(width: 256, height: 256)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ Hike/Hike/Component/MotionAnimationView.swift
@@ -0,0 +1,70 @@
+//
+//  MotionAnimationView.swift
+//  Hike
+//
+//  Created by zsm on 12/1/23.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    @State private var randomCircle: Int = Int.random(in: 6...12)
+    @State private var isAnimating: Bool = false
+    
+    // MARK: - FUNCTIONS
+    // 1. RANDOM COORDINATE
+    func randomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...256)
+    }
+    // 2. RANDOM SIZE
+    func randomSize() -> CGFloat {
+        return CGFloat.random(in: 4...80)
+    }
+    // 3. RANDOM SCALE
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    // 4. RANDOM SPEED
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.05...1.0)
+    }
+    // 5. RANDOM DELAY
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+
+    
+    var body: some View {
+        ZStack{
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundColor(.white)
+                    .opacity(0.25)
+                    .frame(width: randomSize())
+                    .position(
+                        x: randomCoordinate(), y: randomCoordinate())
+                    .scaleEffect(isAnimating ? randomScale() : 1)
+                    .onAppear(perform : {
+                        withAnimation(
+                            .interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                            .repeatForever()
+                            .speed(randomSpeed())
+                            .delay(randomDelay())
+                        ) {
+                            isAnimating = true
+                        }
+                    })
+            }
+        }
+        .frame(width: 256, height: 256)
+        .mask(Circle())
+        // smooth rendering using METAL
+        .drawingGroup()
+    }
+}
+
+#Preview {
+        MotionAnimationView()
+        .background(Circle()
+            .fill(.teal))
+}
