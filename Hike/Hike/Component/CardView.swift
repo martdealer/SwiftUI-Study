1ef73c9d40de4ca1a6732e0a3cd3ae9eeadae1b3
martdealer <ios.martdealer@gmail.com>
2023-12-01 21:01:34 +0900

    cardview

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -8,22 +8,83 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        
+        
+        imageNumber = randomNumber
+    }
+
+    
     var body: some View {
+        // MARK: - CARD
         ZStack {
             CustomBackgroundView()
-               // .padding()
             
-            ZStack {
-                Circle()
-                    .fill(LinearGradient(colors: [
-                        Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
-                                         startPoint: .topLeading,
-                                         endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
+            VStack {
+                // MARK: - HEADER
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            )
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                        }, label: {
+                            CustomButtonView()
+                        })
+                    
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(colors: [
+                            Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
+                                             startPoint: .topLeading,
+                                             endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-\(imageNumber)")
+                        .resizable()
+                        .scaledToFit()
+                        .animation(.default, value: imageNumber)
+                }
+                
+                // MARK: - FOOTER
+                Button{
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom))
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
             }
         }
         .frame(width: 320, height: 570)
