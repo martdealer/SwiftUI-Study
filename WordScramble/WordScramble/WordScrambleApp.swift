28daf16b45b02357e9c1d885c40ea69df830dbe9
martdealer <ios.martdealer@gmail.com>
2023-12-21 20:24:23 +0900

    #1

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by zsm on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
