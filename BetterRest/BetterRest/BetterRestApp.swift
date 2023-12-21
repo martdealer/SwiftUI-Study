d04aee733a9a4406fc473a8ed656f648aa5f3574
martdealer <ios.martdealer@gmail.com>
2023-12-21 13:25:56 +0900

    #1, #challenge

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by zsm on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,112 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by zsm on 12/20/23.
+//
+
+import CoreML
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = "Nothing yet"
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Choose time you want to wake up at") {
+                    HStack {
+                        
+                        Text("Wake-up Time")
+                            
+                        
+                        Spacer()
+                        
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                            .onChange(of: wakeUp) {
+                                calculateBedtime()
+                            }
+                    }
+                }
+                
+                Section("Desired amount of sleep") {
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                        .onChange(of: sleepAmount) {
+                            calculateBedtime()
+                        }
+                }
+                
+                Section("Daily coffee intake") {
+                    
+                    //Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                    
+                    Picker("^[\(coffeeAmount) cup](inflect: true)", selection: $coffeeAmount) {
+                        ForEach(0..<21) {
+                                Text("\($0)")
+                        }
+                    }
+                    .onChange(of: coffeeAmount) {
+                        calculateBedtime()
+                    }
+                        
+                }
+            }
+            .navigationTitle("BetterRest")
+        }
+        VStack {
+            Text("Your ideal bedtime is ")
+            
+            Text("\(alertMessage)")
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        .padding()
+        
+    }
+    
+    func calculateBedtime() {
+        do {
+            
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            showingAlert = true
+
+
+
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
