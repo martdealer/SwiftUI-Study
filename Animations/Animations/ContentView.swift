a2ebcc52c0a529b53754c0c5751509b993cff36a
martdealer <ios.martdealer@gmail.com>
2023-12-26 13:30:29 +0900

    #1

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by zsm on 12/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+
+
+             
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by zsm on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
