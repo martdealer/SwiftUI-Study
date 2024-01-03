e96bbbaa6e0fc31bd2676432ab58f4f2f75536fa
martdealer <ios.martdealer@gmail.com>
2024-01-03 13:41:35 +0900

    Core Image, ContentUnavailableView, App Store review 요청

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by zsm on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
